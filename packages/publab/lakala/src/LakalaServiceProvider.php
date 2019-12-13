73a8bfd5ffbec80f13be69fa2e2b65099f118c32
duanzhiwei <89340545@qq.com>
2019-12-13 11:45:57 +0800

    'lakala'

---
+++ packages/publab/lakala/src/LakalaServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Publab\Lakala;
+
+use Illuminate\Support\ServiceProvider;
+
+class LakalaServiceProvider extends ServiceProvider
+{
+    /**
+     * 服务提供者加是否延迟加载.
+     *
+     * @var bool
+     */
+    protected $defer = true; // 延迟加载服务
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        $this->loadViewsFrom(__DIR__ . '/views', 'lakala'); // 视图目录指定
+//        $this->publishes([
+//            __DIR__.'/views' => base_path('resources/views/vendor/lakala'),  // 发布视图目录到resources 下
+//            __DIR__.'/config/lakala.php' => config_path('lakala.php'), // 发布配置文件到 laravel 的config 下
+//        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // 单例绑定服务
+        $this->app->singleton('lakala', function ($app) {
+            return new Lakala($app['session'], $app['config']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+//    public function provides()
+//    {
+//        // 因为延迟加载 所以要定义 provides 函数 具体参考laravel 文档
+//        return ['lakala'];
+//    }
+}
