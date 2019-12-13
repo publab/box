c9bbb0dc8b9f76c490f25d5c9bc45da9948a9b24
duanzhiwei <89340545@qq.com>
2019-12-13 13:13:14 +0800

    'no-package'

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Publab\Lakala\LakalaServiceProvider::class,
 
     ],
 
@@ -229,7 +228,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Lakala' => Publab\Lakala\Facades\Lakala::class,
 
     ],
 
