013f3ccc6fc66a0bfe858b49ca9f9a19dfd17511
duanzhiwei <89340545@qq.com>
2019-12-23 17:17:42 +0800

    'laravelS-socket'

--- config/laravels.php
+++ config/laravels.php
@@ -20,8 +20,8 @@ return [
     ],
     'event_handlers'           => [],
     'websocket'                => [
-        'enable' => false,
-        //'handler' => XxxWebSocketHandler::class,
+        'enable' => true,
+        'handler' => \App\Services\WebSocketService::class,
     ],
     'sockets'                  => [],
     'processes'                => [
@@ -89,6 +89,10 @@ return [
         'enable_reuse_port'  => true,
         'enable_coroutine'   => false,
         'http_compression'   => false,
+
+        // 每隔 60s 检测一次所有连接，如果某个连接在 600s 内都没有发送任何数据，则关闭该连接
+//        'heartbeat_idle_time'      => 600,
+//        'heartbeat_check_interval' => 60,
 
         // Slow log
         // 'request_slowlog_timeout' => 2,
