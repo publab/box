133bfd2368c5082d08a0ab96ea26ad13057f3580
duanzhiwei <89340545@qq.com>
2020-02-16 00:15:54 +0800

    'bak'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //执行SQL打印
+        DB::listen(function($query) {
+            $bindings = $query->bindings;
+            $sql = $query->sql;
+            foreach ($bindings as $replace){
+                $value = is_numeric($replace) ? $replace : "'".$replace."'";
+                $sql = preg_replace('/\?/', $value, $sql, 1);
+            }
+            info($sql);
+        });
     }
 }
